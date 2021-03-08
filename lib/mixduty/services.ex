c811a11e08238adb619529e7b7b76889a6cb0766
Andrew Summers <asummers@drwholdings.com>
2021-03-08 13:04:03 -0600

    feat: Add ability to create services and integrations for them

--- lib/mixduty/services.ex
+++ lib/mixduty/services.ex
@@ -16,4 +16,46 @@ defmodule Mixduty.Services do
   def list(client, params \\ [], options \\ []) do
     get("#{@path}", client, params, options)
   end
+
+  @doc """
+  Create service
+  #### Example
+      Mixduty.Services.create(client)
+  """
+  def create(name, escalation_policy, client, options \\ %{}) do
+    service_body = %{
+      name: name,
+      escalation_policy: escalation_policy
+    }
+    |> Map.merge(options)
+
+    body = %{service: service_body}
+    client = Map.put(client, :headers, client.headers) # ++ [{"From", from}])
+    post("#{@path}", client, body)
+  end
+
+  @doc """
+  Create an incident note
+  #### Example
+      Mixduty.Services.create_integration("My Integration", "events_api_v2_inbound_integration", "P00PBUG", client)
+  """
+  def create_integration(name, type, service_id, client) do
+    body = %{
+      integration: %{
+        type: type,
+        name: name,
+        service: %{
+          id: service_id,
+          type: "service_reference"
+        }
+      }
+    }
+
+    client = Map.put(client, :headers, client.headers)
+
+    @path
+    |> Path.join(service_id)
+    |> Path.join("integrations")
+    |> post(client, body)
+  end
 end
